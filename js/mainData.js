8ad7fbb5afdb3257424c502f3ce45a3cbd13f469
Stas <stas.shc@gmail.com>
2023-08-30 12:56:15 +0300

    mainData

--- js/mainData.js
+++ js/mainData.js
@@ -35,7 +35,7 @@ const mainData = () => {
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4">
                         <div class="btn__all">
-                            <a href="/categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
+                            <a href="./categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
                         </div>
                     </div>
                 </div>
@@ -59,7 +59,7 @@ const mainData = () => {
                             </div>
                             <div class="product__item__text">
                                 ${tagsBlock.outerHTML}
-                                <h5><a href="/anime-details.html?itemId=${item.id}">${item.title}</a></h5>
+                                <h5><a href="./anime-details.html?itemId=${item.id}">${item.title}</a></h5>
                             </div>
                         </div>
                     </div>
@@ -81,7 +81,7 @@ const mainData = () => {
                 <div class="product__sidebar__view__item set-bg mix" data-setbg="${element.image}">
                     <div class="ep"> ${element.rating} / 10</div>
                     <div class="view"><i class="fa fa-eye"></i> ${element.views} </div>
-                    <h5><a href="/anime-details.html?itemId=${element.id}">${element.title}</a></h5>
+                    <h5><a href="./anime-details.html?itemId=${element.id}">${element.title}</a></h5>
                 </div>
             `)
         });
